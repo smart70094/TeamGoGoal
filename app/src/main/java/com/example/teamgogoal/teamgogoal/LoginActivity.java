0bf79d95ce19b10cf8e8bf2be5cab314ef59a04d
Gary <tp841025@gmail.com>
2017-12-11 19:11:19 +0800

    Merge

--- app/src/main/java/com/example/teamgogoal/teamgogoal/LoginActivity.java
+++ app/src/main/java/com/example/teamgogoal/teamgogoal/LoginActivity.java
@@ -35,7 +35,7 @@ import java.util.Date;
 
 public class LoginActivity extends AppCompatActivity {
     //public static final String localhost="http://169.254.68.146/DB/";
-    public static final String ip = "36.233.130.161";
+    public static final String ip = "192.168.0.100";
     //public static final String ip="111.253.228.128";
     public static final String localhost = "http://" + ip + "/TeamGoGoal/";
     EditText accountTxt, passwordTxt;
